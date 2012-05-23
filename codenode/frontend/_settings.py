a6e3b24460d43fb3a25b5776cabe63fa918cde95
Dorian Raymer <deldotdr@gmail.com>
2012-05-22 22:36:44 -0700

    Update default django template loaders

--- codenode/frontend/_settings.py
+++ codenode/frontend/_settings.py
@@ -75,8 +75,10 @@ SECRET_KEY = 'n1ty3bi2oa-@jed0k@@n6%c4&lhoc$c19a8+82&597sqz&x*8!'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+#    'django.template.loaders.filesystem.load_template_source',
+#    'django.template.loaders.app_directories.load_template_source',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
